4cf7ed80c2b8dc2eedde17ac99f794b2147b93d9
Srflo <srflo65@yahoo.fr>
2020-03-09 14:14:18 +0100

    Ajout de plusieurs échantillons, affichage des min,max,moyenne

--- projet.c
+++ projet.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include "rdtsc.h"
 
+#define NB_SAMPLE 20
+
 void baseline(unsigned n, double a[n], unsigned ind[n], double b[n], double c[n][n]){
     unsigned i,j;
     for(j=0;j<n;j++){
@@ -23,6 +25,16 @@ void init_tab(unsigned n, double a[n], unsigned ind[n], double b[n], double c[n]
         }
     }
 }
+int cmp(const void * a, const void * b){
+    return *(unsigned*)a>*(unsigned*)b;
+}
+double mean(unsigned n, unsigned t[n]){
+    unsigned i,total=0;
+    for(i=0;i<n;i++){
+        total+=t[i];
+    }
+    return total/(double)n;
+}
 
 int main(){
     srand(0);
@@ -30,13 +42,38 @@ int main(){
     double a[n], b[n], c[n][n];
     unsigned ind[n]; 
     
-    unsigned cycles_a,cycles_b;
+    unsigned cycles_a,cycles_b,cycles[NB_SAMPLE];
+    int var_cycle;
     
-    init_tab(n,a,ind,b,c);
+    for(var_cycle=0;var_cycle<NB_SAMPLE;var_cycle++){
+        init_tab(n,a,ind,b,c);
+        
+        cycles_b = rdtsc();
+        baseline(n,a,ind,b,c);
+        cycles_a = rdtsc();
+        
+        cycles[var_cycle]=cycles_a-cycles_b;
+        //printf("%d %d\n",var_cycle,cycles_a-cycles_b);
+
+    }
+    qsort(cycles, NB_SAMPLE,sizeof(unsigned),cmp);
+
+    double min, max, avg, mea, dev, bpc;
+        
+    min = cycles[0];
+    max = cycles[NB_SAMPLE-1];
+    avg = (min+max)/2.0;    
     
-    cycles_b = rdtsc();
-    baseline(n,a,ind,b,c);
-    cycles_a = rdtsc();
+    printf("%f %f\n",min,max);
+        
+    mea = mean(NB_SAMPLE, cycles);
+  
+
+    fprintf(stderr, "\n %u; %f; %f; %f; %f;\n",n,min,max,avg,mea);
     
-    printf("%d\n",cycles_a-cycles_b);
+    //dev = stddev(NB_SAMPLE, cycles);
+    //bpc = size / mea;
+
+    //fprintf(stderr, "\n%20llu; %15.3lf; %15.3lf; %15.3lf; %15.3lf; %15.3lf; %15.3lf %%;\n",(n,min,max,avg,mea,bpc,(dev * 100.0 / mea));
+    
 }
