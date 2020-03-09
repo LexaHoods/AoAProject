be5cb9a097834e169e60993be3351527fc2d350a
Srflo <srflo65@yahoo.fr>
2020-03-09 14:56:20 +0100

    ajout fichier perf, ajout parcours de taille

--- projet.c
+++ projet.c
@@ -53,41 +53,49 @@ double stddev(unsigned n,unsigned t[n]){
 
 int main(){
     srand(0);
-    unsigned n=100;
-    double a[n], b[n], c[n][n];
-    unsigned ind[n]; 
+    
+    FILE* fichier = fopen("perf.csv", "w");  
+    
+    unsigned nmax=800,n;
+    double a[nmax], b[nmax], c[nmax][nmax];
+    unsigned ind[nmax]; 
     
     unsigned cycles_a,cycles_b,cycles[NB_SAMPLE];
     int var_cycle;
     
-    for(var_cycle=0;var_cycle<NB_SAMPLE;var_cycle++){
-        init_tab(n,a,ind,b,c);
+    fprintf(fichier, "    n;             min;             max;             avg;             mea;             bpc;             dev;\n");
+    
+    for(n=10;n<nmax+1;n++){
         
-        cycles_b = rdtsc();
-        baseline(n,a,ind,b,c);
-        cycles_a = rdtsc();
-        
-        cycles[var_cycle]=cycles_a-cycles_b;
-        //printf("%d %d\n",var_cycle,cycles_a-cycles_b);
+        for(var_cycle=0;var_cycle<NB_SAMPLE;var_cycle++){
+            init_tab(n,a,ind,b,c);
+            
+            cycles_b = rdtsc();
+            baseline(n,a,ind,b,c);
+            cycles_a = rdtsc();
+            
+            cycles[var_cycle]=cycles_a-cycles_b;
+            //printf("%d %d\n",var_cycle,cycles_a-cycles_b);
 
+        }
+        qsort(cycles, NB_SAMPLE,sizeof(unsigned),cmp);
+
+        double min, max, avg, mea, dev, bpc;
+            
+        min = cycles[0];
+        max = cycles[NB_SAMPLE-1];
+        avg = (min+max)/2.0;    
+        
+        //printf("%f %f\n",min,max);
+            
+        mea = mean(NB_SAMPLE, cycles);
+      
+        dev = stddev(NB_SAMPLE, cycles);
+        bpc = n / mea;
+        
+        
+        fprintf(fichier, "%5u; %15.3f; %15.3f; %15.3f; %15.3f; %15.3f; %15.3f;\n",n,min,max,avg,mea,bpc,(dev*100.0/mea));
     }
-    qsort(cycles, NB_SAMPLE,sizeof(unsigned),cmp);
 
-    double min, max, avg, mea, dev, bpc;
-        
-    min = cycles[0];
-    max = cycles[NB_SAMPLE-1];
-    avg = (min+max)/2.0;    
-    
-    printf("%f %f\n",min,max);
-        
-    mea = mean(NB_SAMPLE, cycles);
-  
-    dev = stddev(NB_SAMPLE, cycles);
-    bpc = n / mea;
-    
-    fprintf(stderr, "\n     n;             min;             max;             avg;             mea;             bpc;             dev;");
-    fprintf(stderr, "\n %5u; %15.3f; %15.3f; %15.3f; %15.3f; %15.3f; %15.3f;\n",n,min,max,avg,mea,bpc,(dev*100.0/mea));
-    
-
+    fclose(fichier);
 }
